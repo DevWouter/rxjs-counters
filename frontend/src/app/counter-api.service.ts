d8de6cc30026a08b3ea5b82fe9bdccf6b4d7d0d1
Wouter Lindenhof <wouterlindenhof@hey.com>
2024-04-09 23:13:41 +0200

    Bad live update

--- frontend/src/app/counter-api.service.ts
+++ frontend/src/app/counter-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, finalize, switchMap  } from 'rxjs';
 
 export interface Counter {
   name: string;
@@ -16,12 +16,22 @@ export class CounterApiService {
     private readonly httpClient: HttpClient
   ) { }
 
+  /**
+   * Using a BehaviorSubject to trigger a reload of all the requests. We do this in the service so that all views are updated.
+   * This is a **TERRIBLE** idea unless you layer the service.
+   */
+  private reload$ = new BehaviorSubject(null);
+
+
   all() : Observable<string[]> {
-    return this.httpClient.get<string[]>('/api/counters');
+    return this.reload$.pipe(
+      switchMap(()=> this.httpClient.get<string[]>('/api/counters'))
+    );
   }
 
   create(name: string) :Observable<never> {
-    return this.httpClient.post<never>('/api/counters', { name });
+    return this.httpClient.post<never>('/api/counters', { name })
+    .pipe(finalize(() => this.reload$.next(null)));
   }
 
   get(name:string): Observable<Counter> {
@@ -29,10 +39,12 @@ export class CounterApiService {
   }
 
   delete(name:string): Observable<number> {
-    return this.httpClient.delete<number>('/api/counters/' + name);
+    return this.httpClient.delete<number>('/api/counters/' + name)
+    .pipe(finalize(() => this.reload$.next(null)))
   }
 
   update(name:string, value:number) {
-    return this.httpClient.put('/api/counters/' + name, {value});
+    return this.httpClient.put('/api/counters/' + name, {value})
+    .pipe(finalize(() => this.reload$.next(null)))
   }
 }
